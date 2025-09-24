3d827c41e7124da90010d6e6df75fdfa5f795702
adeennour4-dot <adeennour4@gmail.com>
2025-09-25 02:48:19 +0300

    Update PdfExporter.java

--- app/src/main/java/com/medicaltranslator/PdfExporter.java
+++ app/src/main/java/com/medicaltranslator/PdfExporter.java
@@ -1,44 +1,44 @@
-
 package com.medicaltranslator;
 
 import android.content.Context;
-import android.os.Environment;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.text.PDFTextStripper;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class PdfExporter {
-    public static File exportWithTranslations(Context context, File originalPdfFile,
-                                              List<String> translationsPerPage,
-                                              List<String> glossaryLines) throws Exception {
+
+    public static File exportWithTranslation(Context context, File originalPdfFile, List<String> translationPages, List<String> glossaryLines) throws IOException {
+
         PDDocument original = PDDocument.load(originalPdfFile);
         PDDocument output = new PDDocument();
 
-        PDType0Font fallbackFont = null;
-        try {
-            InputStream is = context.getAssets().open("fonts/Roboto-Regular.ttf");
+        PDFont fallbackFont = null;
+        try (InputStream is = context.getAssets().open("fonts/Roboto-Regular.ttf")) {
             fallbackFont = PDType0Font.load(output, is, true);
-        } catch (Exception e) {}
+        } catch (IOException e) {
+            // Handle font loading error
+        }
 
-        int numPages = original.getNumberOfPages();
-
-        for (int i = 0; i < numPages; i++) {
-            PDPage origPage = original.getPage(i);
-            output.addPage(origPage);
+        for (int i = 0; i < translationPages.size() && i < original.getNumberOfPages(); i++) {
+            String translationText = translationPages.get(i);
+            PDPage originalPage = original.getPage(i);
             PDPage transPage = new PDPage(PDRectangle.A4);
             output.addPage(transPage);
-            String translationText = (i < translationsPerPage.size()) ? translationsPerPage.get(i) : "";
+
+            String stringTranslationText = i < translationPages.size() ? translationPages.get(i) : "";
             PDPageContentStream pcs = new PDPageContentStream(output, transPage);
             pcs.beginText();
-            if (fallbackFont != null) pcs.setFont(fallbackFont, 12);
-            pcs.newLineAtOffset(40, 750);
-            String[] lines = translationText.split("\\n");
+            pcs.setFont(fallbackFont, 12);
+            pcs.newLineAtOffset(60, 750);
+            String[] lines = translationText.split("\n");
             for (String line : lines) {
                 pcs.showText(line);
                 pcs.newLineAtOffset(0, -14);
@@ -52,8 +52,8 @@ public class PdfExporter {
             output.addPage(glossaryPage);
             PDPageContentStream pcs = new PDPageContentStream(output, glossaryPage);
             pcs.beginText();
-            if (fallbackFont != null) pcs.setFont(fallbackFont, 12);
-            pcs.newLineAtOffset(40, 750);
+            pcs.setFont(fallbackFont, 12);
+            pcs.newLineAtOffset(60, 750);
             for (String line : glossaryLines) {
                 pcs.showText(line);
                 pcs.newLineAtOffset(0, -14);
